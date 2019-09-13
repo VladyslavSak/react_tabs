91085a354e97e28007c5dbe509bc27df89ce067c
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-14 01:27:02 +0300

    Updates

---
+++ precache-manifest.896247f98eb857a7e9849c84a4d67ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7fc683254f62a595607e60bafc6af6",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "265f1bc93680016b8fa3",
+    "url": "/react_tabs/static/css/main.42cd5796.chunk.css"
+  },
+  {
+    "revision": "b537c28de8df8107fe00",
+    "url": "/react_tabs/static/js/2.052be353.chunk.js"
+  },
+  {
+    "revision": "265f1bc93680016b8fa3",
+    "url": "/react_tabs/static/js/main.e20cc45f.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
